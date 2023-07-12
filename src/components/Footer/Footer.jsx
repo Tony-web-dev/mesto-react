8c60224e1acf95653ac5b251ae80195e73447e31
Tony_T <antoninatimush@yandex.ru>
2023-07-12 18:19:55 +0300

    all

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__copyright">© 2023 Антонина Тимуш</p>
+      <p className="footer__copyright">© 2023 Project Mesto</p>
     </footer>
   );
 }
